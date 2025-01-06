c395aeb2223ce0733375de60e0600cc80a0d4163
Meesz <20428509+Meesz@users.noreply.github.com>
2025-01-06 01:45:36 +0100

    Refactor Post model to include user_id in fillable and add user relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'order'];
+    protected $fillable = ['title', 'content', 'order', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
